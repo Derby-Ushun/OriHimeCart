ed0926bf71dca95f4249e7d78cd8598bd3358db8
Derby-Ushun <maedayushun@gmail.com>
2024-04-15 22:17:31 +0900

    20240415_ver1.3.1 OriHimeCart-TX仕様に最大速度の変更

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ Action ACTIONS[] = {
 
 
 // ---- S/W Version ------------------
-#define VERSION_NUMBER  "  Ver. 1.3.0"
+#define VERSION_NUMBER  "  Ver. 1.3.1"
 // -----------------------------------
 
 ////////PIN番号////////
